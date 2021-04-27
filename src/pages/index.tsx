a13b7a22883020a1eacb7f7d63626afd3f6c5852
Erez Or <erez180103@gmail.com>
2021-04-28 01:10:13 +0300

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import { Input, Stack } from "@chakra-ui/react";
+import { Button, Flex, Input, Stack, Text } from "@chakra-ui/react";
 // import { GetServerSideProps } from "next";
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 import AttackCard from "../components/AttackCard";
 import Layout from "../components/Layout";
-import PageNavigator from "../components/PageNavigator";
 import { TAKE } from "../constants";
 import { AttackFetcherProps, fetchAttacks } from "../fetchers/atttackFetcher";
 import { Attack } from "../types/attack";
@@ -33,14 +32,6 @@ const Index: React.FC<AttackFetcherProps> = (props) => {
       };
     });
 
-  const updateVariabless = (vars: PaginationType) => {
-    setVariabless({
-      take: vars.take,
-      skip: vars.skip,
-      keyword: vars.keyword,
-    });
-  };
-
   return (
     <Layout>
       <Input
@@ -55,24 +46,68 @@ const Index: React.FC<AttackFetcherProps> = (props) => {
         <div>loading...</div>
       ) : data ? (
         data.error ? (
-          <div>{data.error}</div>
+          <div>Error {data.error}</div>
         ) : data.data ? (
           <div>
             <Stack p={8}>
-              {data.data.map((attack: Attack) => (
+              {data?.data.map((attack: Attack) => (
                 <AttackCard attack={attack}></AttackCard>
               ))}
             </Stack>
-            <PageNavigator
-              updateVariabless={updateVariabless}
-              vars={variabless}
-              count={data.count}
-              isPreviousData={isPreviousData}
-            />
+            {variabless.skip < data.count ? (
+              <Flex
+                mt={4}
+                mb={2}
+                position={"sticky"}
+                bottom={0}
+                bg={"tan"}
+                width={"full"}
+                p={2}
+              >
+                <Text m={"auto"}>
+                  Current Page: {variabless.skip / TAKE + 1}
+                </Text>
+                <Button
+                  size={"md"}
+                  m={"auto"}
+                  onClick={() =>
+                    setVariabless((old) => {
+                      return {
+                        take: old.take,
+                        skip: old.skip - TAKE,
+                        keyword: old.keyword,
+                      };
+                    })
+                  }
+                  disabled={variabless.skip < TAKE}
+                >
+                  Previous Page
+                </Button>
+                <Button
+                  size={"md"}
+                  m={"auto"}
+                  onClick={() => {
+                    if (!isPreviousData && data.count > variabless.skip) {
+                      setVariabless((old) => {
+                        return {
+                          take: old.take,
+                          skip: old.skip + old.take,
+                          keyword: old.keyword,
+                        };
+                      });
+                    }
+                  }}
+                  // Disable the Next Page button until we know a next page is available
+                  disabled={isPreviousData || data.count < variabless.skip}
+                >
+                  Next Page
+                </Button>
+              </Flex>
+            ) : null}
           </div>
         ) : null
       ) : (
-        <div>query error</div>
+        <div>Query error</div>
       )}
     </Layout>
   );
